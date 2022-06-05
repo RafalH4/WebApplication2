cbcb043eca2dc7746bd3d7f1e687d7019e72e7ac
Rafał Huk <r.huk@outlook.com>
2022-06-05 13:28:37 +0200

    Add project files.

---
+++ WebApplication2/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Page> Pages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Document>().ToTable("Document");
+            modelBuilder.Entity<Page>().ToTable("Page");
+        }
+
+    }
+}
